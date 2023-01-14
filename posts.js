8dea1d18655548bff4a1235976f53b38abe1e4b4
Emanuel Douglas <emanuel.douglas.sc@gmail.com>
2023-01-14 18:23:52 -0300

    Writing post 1

--- posts.js
+++ posts.js
@@ -8,26 +8,33 @@ const pathJsonFiles = `${pathFiles}/jsonposts`
 
 const files = fs.readdirSync(pathMdFiles, 'utf8')
 
+const jsonNamesGenerated = []
+
 const listAllPosts = files.map(fileName => {
     const file = fs.readFileSync(`${pathMdFiles}/${fileName}`, 'utf-8')
     const { data, content } = matter(file)
-    console.log(data)
     const jsonFileData = { data, content }
     const pathToSavePost = `${pathJsonFiles}/${data.slug}.json`
-    fs.writeFileSync(pathToSavePost, JSON.stringify(jsonFileData), { encoding: 'utf-8' })
+    jsonNamesGenerated.push(pathToSavePost)
+    fs.writeFileSync(
+        pathToSavePost, 
+        JSON.stringify(jsonFileData), 
+        { encoding: 'utf-8' }
+    )
     return data
 })
 
 
-fs.writeFileSync(`${pathJsonFiles}/listAllPosts.json`, JSON.stringify(listAllPosts), { encoding: 'utf-8' })
+fs.writeFileSync(
+    `${pathJsonFiles}/listAllPosts.json`,
+    JSON.stringify(listAllPosts),
+    { encoding: 'utf-8' }
+)
 
+var finishMessage = `generated files:\n\t${pathJsonFiles}/listAllPosts.json\n`
 
+jsonNamesGenerated.forEach(filename => {
+    finishMessage += `\t${filename}\n`
+})
 
-
-// const jsonData = data
-
-// const endPathName = `${pathFiles}/mdfiles.json`
-
-// fs.writeFileSync(endPathName, JSON.stringify(jsonData), { encoding: 'utf-8' })
-
-// console.log('...finished')
+console.log(finishMessage)
