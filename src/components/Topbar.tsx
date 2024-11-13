6e0748cae93407306826408d8c23a33758cf901c
Emanuel Costa <amcom-emanuel.costa@ailos.coop.br>
2024-11-13 09:35:06 -0300

    ajustanto evento e estilo do menu

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,7 +1,9 @@
 import Link from 'next/link';
+import { useState } from 'react';
 
 export default function Topbar({ currentPath }: { currentPath: string }) {
-  const isActive = (path: string) => currentPath === path;
+
+  const [active, useActive] = useState('/')
 
   return (
     <header className="bg-neutral-light shadow">
@@ -9,28 +11,31 @@ export default function Topbar({ currentPath }: { currentPath: string }) {
         <div className="flex justify-between h-16">
           <div className="flex">
             <div className="flex-shrink-0 flex items-center">
-              <Link href="/">
-                <span className="text-xl font-semibold text-primary">Types EDSC</span>
+              <Link href="/" onClick={() => useState('/')}>
+                <span className="text-xl font-semibold text-primary">EDSC.GitHub</span>
               </Link>
             </div>
             <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
               <Link
                 href="/"
-                className={`${isActive('/') ? 'border-primary text-primary' : 'border-transparent text-neutral-dark hover:text-primary'
+                onClick={() => useState('/')}
+                className={`${active === '/' ? 'border-primary text-primary' : 'border-transparent text-neutral-dark hover:text-primary'
                   } inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium`}
               >
                 Home
               </Link>
               <Link
                 href="/blog"
-                className={`${isActive('/blog') ? 'border-primary text-primary' : 'border-transparent text-neutral-dark hover:text-primary'
+                onClick={() => useState('/blog')}
+                className={`${active === '/blog' ? 'border-primary text-primary' : 'border-transparent text-neutral-dark hover:text-primary'
                   } inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium`}
               >
                 Blog
               </Link>
               <Link
                 href="/about"
-                className={`${isActive('/about') ? 'border-primary text-primary' : 'border-transparent text-neutral-dark hover:text-primary'
+                onClick={() => useState('/about')}
+                className={`${active === '/about' ? 'border-primary text-primary' : 'border-transparent text-neutral-dark hover:text-primary'
                   } inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium`}
               >
                 Sobre
